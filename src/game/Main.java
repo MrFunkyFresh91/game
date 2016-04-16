54d2f7520ff390a31a30c8dfd9fd4de03ac09ce0
MrFunkyFresh91 <mrfunkyfresh91@gmail.com>
2016-04-16 18:02:46 +0200

    Slick2D Setup

---
+++ src/game/Main.java
@@ -0,0 +1,47 @@
+package game;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Main extends BasicGame {
+
+	public Main() {
+		super("MyGame");
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		try
+        {
+            AppGameContainer app = new AppGameContainer(new Main());
+            app.setDisplayMode(500, 400, false);
+            app.start();
+        }
+        catch (SlickException e)
+        {
+            e.printStackTrace();
+        }
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
